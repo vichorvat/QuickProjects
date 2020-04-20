c97419b465d37ab3f9c195566009d9936d05228b
Victor Horvat <u6950099@anu.edu.au>
2020-04-20 18:14:35 +1000

    A recursive function that takes input and then prints out a triangle of numbers where each new line increases in length by one until as long as the input. and the input is printed but decreases by value in 1 until it is the value 1.

---
+++ .idea/triangle_of_numbers.py
@@ -0,0 +1,24 @@
+# Write a program that prints a pattern like this
+# 7
+# 66
+# 555
+# 4444
+# 33333
+# 222222
+# 1111111
+#
+# Credit to @programmingtask on instagram for the task
+
+inputNum = int(input("input: "))
+
+def mainFuncHelper(outputNum, amount):
+    if amount <= inputNum:
+        output = ""
+        for i in range(0,amount):
+            output = output + str(outputNum)
+        print(output)
+        mainFuncHelper((outputNum - 1), (amount + 1) )
+
+
+mainFuncHelper(inputNum, 1)
+
